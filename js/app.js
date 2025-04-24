75c79be970e6d81338b4186b921e650fbebd41bc
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-24 18:47:18 +0200

    function intro

--- js/app.js
+++ js/app.js
@@ -1,20 +1,11 @@
-var person1 = {
-    firstName: 'Mateusz',
-    lastName: 'Choma',
+
+var x = 5
+var y = 10
+
+function add(a, b) {
+    return a + b
 }
+const result = add(x, y)
 
-var person2 = {
-    firstName: 'Alicja',
-    lastName: 'Kotowicz',
-}
+console.log(result);
 
-var people = [person1, person2, person2]
-
-person2.firstName = "Ala"
-
-people[2].name = people[2].firstName + " " + people[2].lastName
-
-console.log(person1);
-console.log(person2);
-
-console.log(people);
