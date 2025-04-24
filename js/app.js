52012f7526861c8fae5bdde3603db788b914a996
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-24 16:43:29 +0200

    coercion cdn

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-const string = '4'
-const num = 4
-console.log(string + num); // 44
+var x = 2
+var y = 4
+
+var a = '2'
+var b = '4'
+
+console.log(x + y); // 6
+
+console.log(a + b); // 24
+
+console.log(a - b); // -2
+
+// podczas dodawania zachodzi konwersja liczb na string, 
+// natomiast przy innych operacjach arytmetycznych stringi sa zamieniane na liczby
+
+
