f68adcf9d3814d437e8a7a792e913412eba3de5c
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-24 15:29:30 +0200

    obiekt globalny window i globalny zakres

--- js/app.js
+++ js/app.js
@@ -1,36 +1,7 @@
-// PRIMITIVES:
-
-// NUMBER
-var numberLiteral = 5
-var numberLiteral2 = 1.5
-// STRING
-var stringLiteral = 'Hello'
-var stringLiteral2 = "I can't"
-
-//BOOLEAN
-var booleanLiteral = true
-var booleanLiteral2 = false
-
-// NULL
-var nullLiteral = null
-
-// UNDEFINED
-var undefinedLiteral = undefined
-
-// OBJECTS:
+console.log('Hello!')
+console.log(console);
+console.log(console.log);
 
 
-// OBJECT
-var objectLiteral = {
-    name: 'Fluffy',
-    age: 3,
-}
-
-// ARRAY
-var arrayLiteral = ['Fluffy', 3]
-
-// FUNCTIONS
-
-function sayHello(){
-    console.log('Hello!');
-}
+var console2 = 'Hello!'
+console.log(console);
