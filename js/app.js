450f620d97bd58219e323e963b1602e005d895b7
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-24 14:08:22 +0200

    data types

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+// PRIMITIVES:
+
+// NUMBER
+5
+
+// STRING
+'Hello'
+"I can't"
+
+//BOOLEAN
+true
+false
+
+// NULL
+null
+
+// UNDEFINED
+undefined
+
+// OBJECTS:
+
+
+// OBJECT
+const obj = {
+    name: 'Fluffy',
+    age: 3,
+}
+
+// ARRAY
+['Fluffy', 3]
+
+// FUNCTIONS
+
+function sayHello(){
+    console.log('Hello!');
+}
