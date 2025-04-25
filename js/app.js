dac0730712c8bfc74c8149a8efe37c98bbbe8e1d
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-25 15:16:51 +0200

    for loop

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
 'use strict'
 
-;(function(){
-    var num1 = Number(prompt('Please type number!'))
-    var num2 = Number(prompt('Please type second number!'))
-    
-    function add(a, b){
-        return a + b
-    }
-    
-    var sum = add(num1, num2)
-    
-    if(sum || sum === 0){
-        alert('Result: ' + sum);
-    } else {
-            alert('Sorry, you provided not a number!')
-    }
-})()
+var text = 'Hello! Times: '
+var endInc = 10
 
+for(var i = 1; i <= endInc; i++){
+    console.log(text + i)
+}
