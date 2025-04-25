0258c8e2a6aadd4c71645690687cccf424a09894
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-25 16:21:05 +0200

    break & continue

--- js/app.js
+++ js/app.js
@@ -1,30 +1,35 @@
 'use strict'
 
-;(function(numbersAmount){
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-var sum = 0
+// for(let i = 0; i < numbers.length; i++){
+//     console.log('#1. Start of iteration: ' + i);
 
-for(let i=0; i < numbersAmount; i++){
-    var numberFromUser = getNumberFromUser()
-    sum = add(sum, numberFromUser)
+//     var number = numbers[i]
+//     if(number < 6){
+//         console.log(number);
+//     }
+    
+// }
+
+// for(var j = 0; j < numbers.length; j++){
+//     console.log('#2. Start of iteration: ' + j);
+//     var number = numbers[j]
+
+//     if(number < 6){
+//         console.log(number);
+//     } else {
+//         break
+//     }
+// }
+
+for(var k = 0; k < numbers.length; k++){
+    console.log('#3. Start of iteration: ' + k);
+    var number = numbers[k]
+
+    if(number % 2 === 0){
+        continue
+    } 
+    
+    console.log(number);
 }
-
-function getNumberFromUser(){
-    return Number(prompt('Please type number!'))
-}
-
-function add(a, b){
-    return a + b
-}
-
-function showResult(sum){
-    if(sum || sum === 0){
-        alert('Result: ' + sum);
-    } else {
-            alert('Sorry, you provided not a number!')
-    }
-}
-
-showResult(sum)
-})(2)
-
