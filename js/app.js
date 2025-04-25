75b0e3a88011de5fe1054d50d6da15bc0aa7fa13
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-25 14:59:18 +0200

    IIFE - in practice

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 'use strict'
 
-var result = (function add(a, b){
-    return a + b
-})(1, 3)
+;(function(){
+    var num1 = Number(prompt('Please type number!'))
+    var num2 = Number(prompt('Please type second number!'))
+    
+    function add(a, b){
+        return a + b
+    }
+    
+    var sum = add(num1, num2)
+    
+    if(sum || sum === 0){
+        alert('Result: ' + sum);
+    } else {
+            alert('Sorry, you provided not a number!')
+    }
+})()
 
-
-// var result = add(1, 3)
-console.log(result);
