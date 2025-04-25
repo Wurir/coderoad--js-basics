a21ea284c3fbc426004c4a9048148064d93aa877
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-25 18:15:29 +0200

    calculator project

--- js/app.js
+++ js/app.js
@@ -1,17 +1,75 @@
 'use strict'
 
-console.log(a) // undefined
-
-var a = 5
-
-console.log(add(1,3)) // 4
-
-function add(a, b){
-    return a + b
-}
-
-console.log(addExpression(1, 3)) // Uncaught TypeError: addExpression is not a function
-
-var addExpression = function(a,b){
-    return a+ b
-}
+var startCalculator = (function (){
+    function showResult(result){
+        alert('Result is: ' + result)
+    }
+    
+    function getNumberFromUser(){
+        var num = Number(prompt('Type number!'))
+        if(Number.isNaN(num)){
+           return getNumberFromUser()
+        }else {
+            return num
+        }
+    }
+    
+    function getOperatorFromUser(){
+        var input = prompt('Type operator (+, -, *, /')
+        switch(input){
+            case '+':
+                return input
+            case '-':
+                return input
+            case '*':
+                return input
+            case '/':
+                return input
+            default:
+                return getOperatorFromUser()
+        }
+    }
+    
+    function getConfirmationFromUser(){
+        return confirm('Do you want to proceed?')
+    }
+    
+    function getConfirmationFromUserAndProceed(result){
+        var confirmation = getConfirmationFromUser()
+    
+        if(confirmation){
+            mainLoop(result)
+        }else{
+            showResult(result)
+        }
+    }
+    
+    function calculate(operator, a, b){
+        switch(operator){
+            case '+':
+                return a + b
+            case '-':
+                return a - b
+            case '*':
+                return a * b
+            case '/':
+                return a / b
+        }
+    }
+    
+    function mainLoop(result){
+        var operator = getOperatorFromUser()
+        var number = getNumberFromUser()
+    
+        var newResult = calculate(operator, result, number)
+    
+        getConfirmationFromUserAndProceed(newResult)
+    }
+    
+    function start(){
+        var result = getNumberFromUser()
+        
+        getConfirmationFromUserAndProceed(result)
+    }
+    return start
+})()
