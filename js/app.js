b489b6d46cac225cecd33a78713653fda980145a
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-26 15:55:36 +0200

    functions as object, bad solution explained

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
-'use strict'
+const makeCounter = function(name){
+    return {
+        name: name,
+        number: 0,
+        inc: makeCounter.inc,
+        status: makeCounter.status,
+    }
+}
 
-const inc = function(){
+
+// this is not the best solution, constructor functions ar better
+makeCounter.inc = function(){
     this.number++
 }
-    
-const status = function(){
+makeCounter.status = function(){
     console.log('Current number is: ' + this.number);
+    return this.number
 }
 
-const counter1 = {
-        number: 0,
-        inc: inc,
-        status: status,
-    }
-const counter2 = {
-        number: 0,
-        inc: inc,
-        status: status,
-    }
+const counter1 = makeCounter('counter1')
+const counter2 = makeCounter('counter2')
+
+counter1.inc()
+counter2.inc()
+counter2.inc()
+
+if(counter1.status() !== 1 ) throw new Error('Counter1 do not work')
+if(counter2.status() !== 2 ) throw new Error('Counter2 do not work')
