f1224e8903ebe75c926379609f9e245833e38f1d
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-26 14:57:54 +0200

    referencja w obiektach

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const obj = {}
+console.log(obj); // {}
+
+obj.firstName = 'Bartek'
+console.log(obj); // {firstName: 'Bartek'}
+
+const obj2 = obj
+obj2.firstName = 'Mateusz'
+console.log(obj); // {firstName: 'Mateusz'}
+
+console.log(obj2 === obj); // true
