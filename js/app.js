830298fa516c561a720a70ce3ea9b0992320b9a9
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-29 17:29:22 +0200

    class rules

--- js/app.js
+++ js/app.js
@@ -1,49 +1,23 @@
-function Counter (initialNumber = 0) {
-    this.number = initialNumber
+try {
+    const counter1 = new Counter()
+} catch (error) {
+    console.error(error)
 }
 
-Counter.prototype.inc = function () {
-    this.number = this.number + 1
-}
-Counter.prototype.dec = function () {
-    this.number = this.number - 1
-}       
+const Counter = class Counter {
 
-const counter1 = new Counter(5)
+    // this is NOT a valid JS!
+    // but works in most modern browsers (eg. Chrome)
+    number = 0
 
-console.log(typeof Counter)
-console.log(Counter.prototype)
-console.log(Counter.prototype.constructor === Counter)
-
-// it can be call without new
-// but it is not a good pratcie
-Counter()
-
-class CounterClass {
-
-    constructor(initialNumber = 0) {
-        this.number = initialNumber
-    }
-
-    inc() {
+    // this won't go to the prototype!
+    // this will be internal object property as number above
+    inc = () => {
         this.number = this.number + 1
     }
 
 }
 
-CounterClass.prototype.dec = function () {
-    this.number = this.number - 1
-}
+const counter2 = new Counter()
 
-const counter2 = new CounterClass(5)
-
-console.log(typeof CounterClass)
-console.log(CounterClass.prototype)
-console.log(CounterClass.prototype.constructor === CounterClass)
-
-// can't call without new!
-try {
-    CounterClass()
-} catch (error) {
-    console.error(error)
-}
+console.log(counter2)
